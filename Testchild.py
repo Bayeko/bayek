ff3bbe1372d51a236bc682eb1a30d47d50113950
Bayeko <messadene.amir@gmail.com>
2024-06-25 19:57:53 +0200

    Update Testchild.py

--- Testchild.py
+++ Testchild.py
@@ -1,2 +1,59 @@
-print("Inside Child Branch")
-Print("Lessgooo")
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Data Science Tools and Ecosystem**
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# **Objectives** :
+# - Python
+# - R
+# - SQL
+# - Scala
+# - Julia
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. Scikit-learn
+# 
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | VS Code            |
+
+# Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[17]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+Result= (3*4)+5
+Result
+
+
+# In[23]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+hours=200/60
+hours
+
+
+# ## Author 
+# Amir
+# 
+
+# In[ ]:
+
+
+
